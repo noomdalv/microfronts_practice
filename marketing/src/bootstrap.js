bca81b7cdcfd67d7d97dc93384f17ee67b7253ae
noomdalv <vladlunasan@gmail.com>
2023-02-06 01:53:28 -0500

    added landing and pricing components, setup container

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
 // Mount function to start up the app
 
+
 const mount = (el) => {
     ReactDOM.render(
-        <h1>Hi there</h1>, el
+        <App />, el
     )
 }
 
